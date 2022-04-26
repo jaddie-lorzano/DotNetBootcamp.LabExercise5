04f36ce38b8e11b6c523cd9e2bd48ecfe7e72e48
jaddielorzano-pointwest <100465576+jaddielorzano-pointwest@users.noreply.github.com>
2022-04-26 10:39:22 +0800

    Upload C# Lab Exercise 5

---
+++ CSharp.LabExercise5/DriveVehicle.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharp.LabExercise5.Solution4
+{
+    internal class DriveVehicle
+    {
+        public void Start()
+        {
+            RunMainMenu();
+            Drive();
+            Console.ReadLine();
+        }
+        private static void RunMainMenu()
+        {
+            string prompt = @"
+
+________       _____                                  
+___  __ \_________(_)__   ______                      
+__  / / /_  ___/_  /__ | / /  _ \                     
+_  /_/ /_  /   _  / __ |/ //  __/                     
+/_____/ /_/    /_/  _____/ \___/                      
+                                                      
+            ___    __    ______ _____      ______     
+______ _    __ |  / /_______  /____(_)________  /____ 
+_  __ `/    __ | / /_  _ \_  __ \_  /_  ___/_  /_  _ \
+/ /_/ /     __ |/ / /  __/  / / /  / / /__ _  / /  __/
+\__,_/      _____/  \___//_/ /_//_/  \___/ /_/  \___/ 
+                                                      
+";
+            string[] options = { "Drive", "Exit Program" };
+            Menu mainMenu = new Menu(prompt, options);
+            int SelectedIndex = mainMenu.Run();
+
+
+            switch (SelectedIndex)
+            {
+                case 0:
+                    Car car = new Car();
+                    break;
+                case 1:
+                    MainMenu myMainMenu = new MainMenu();
+                    myMainMenu.Start();
+                    break;
+            }
+            Console.ReadKey();
+            DriveVehicle driveVehicle = new DriveVehicle();
+            driveVehicle.Start();
+        }
+        public static void Drive()
+        {
+            _ = new Car();
+        }
+    }
+    interface IVehicle
+    {
+        void Drive();
+        bool Refuel(int amount);
+    }
+
+    public class Car : IVehicle
+    {
+        public int Fuel { get; set; }
+        public Car()
+        {
+            Drive();
+        }
+        public Car(int fuel)
+        {
+            this.Fuel = fuel;
+        }
+        public void Drive()
+        {
+            if (this.Fuel == 0)
+            {
+                AskToGetFuel();
+                Drive();
+            }
+            if (Fuel > 0)
+            {
+                while(Fuel > 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine(@"                                     
+                                       ,&@&&&&&&&&@&/                           
+                                 &&&                      *&&&&                 
+                           /&&,                                     .#%.        
+                 /@&%(                                   .#@%,                  
+           ,%&.  (@&&@,                             &@.     /@&&@*        /     
+        @&    &,        @%                      %&.      @(        #@     %     
+     /%      @            &                             &            &  .,%     
+      %     /#            &*,***,***************,,**,,**@            @,         
+             &/          &,                             %%          &(          
+               &@,    #&*                                 %&/    (&#            
+                                                                      
+");
+                    Console.WriteLine("The car is driving");
+                    Fuel--;
+                    Console.WriteLine($"The fuel amount is {Fuel}");
+                    Thread.Sleep(1000);
+                }
+                Drive();
+            }
+        }
+        public bool Refuel(int amount)
+        {
+            this.Fuel += amount;
+            Console.WriteLine(
+                $"Refueled {amount}. " +
+                $"Your fuel now is {this.Fuel}. " +
+                $"You may now proceed driving..\n");
+            Console.ReadKey();
+            Drive();
+            if (this.Fuel >= 0) return true;
+            return false;
+        }
+        public void AskToGetFuel()
+        {
+            Console.WriteLine("You are out of fuel.");
+            Console.WriteLine("Would you like to Refuel? [y/n]");
+            string answer = Console.ReadLine();
+            while (true)
+            {
+                if (answer.ToLower() == "y")
+                {
+                    int fuelAmount = EnterFuelAmount();
+                    Refuel(fuelAmount);
+                    Drive();
+                }
+                if (answer.ToLower() == "n")
+                {
+                    DriveVehicle driveVehicle = new DriveVehicle();
+                    driveVehicle.Start();
+                }
+            }
+        }
+        public static int EnterFuelAmount()
+        {
+            Console.Write("Enter the amount of Fuel: ");
+            int fuelAmount = Convert.ToInt32(Console.ReadLine());
+            return fuelAmount;
+        }
+    }
+}
---
+++ CSharp.LabExercise5/ListPersonNames.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace CSharp.LabExercise5.Solution1
+{
+    static class UserInputValidator
+    {
+        public static string ValidateUserInput(string firstOrLastName)
+        {
+            string userFirstNameInput;
+
+            while (true)
+            {
+                PersonArrayRenderer.GenerateImage();
+                Console.Write($"\n  Enter {firstOrLastName}: ");
+                userFirstNameInput = Console.ReadLine();
+
+                if (int.TryParse(userFirstNameInput, out _) 
+                    || userFirstNameInput == null 
+                    || userFirstNameInput.Trim().Equals(""))
+                {
+                    Console.WriteLine($"  Invalid input. Please enter {firstOrLastName}.\n");
+                    continue;
+                }
+                break;
+            }
+
+            return userFirstNameInput;
+        }
+    }
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName}";
+        }
+    }
+
+    class PersonArrayRenderer
+    {
+        IPersonArrayCreator personArrayCreator;
+
+        public static void GenerateImage()
+        {
+            Console.Clear();
+            string people = @"
+                                   .                                  
+        .@@@@@@&             .@@@@@@@@@@@.             &@@@@@@.       
+      @@@@@@@@@@@@          @@@@@@@@@@@@@@@          @@@@@@@@@@@@     
+     %@@@@@@@@@@@@@        (@@@@@@@@@@@@@@@#        @@@@@@@@@@@@@&    
+     *@@@@@@@@@@@@@         @@@@@@@@@@@@@@@.        @@@@@@@@@@@@@/    
+      .@@@@@@@@@@@           @@@@@@@@@@@@@           @@@@@@@@@@@,     
+          %@@@*                /@@@@@@@(                *@@@%         
+   @@@@@@&,  ./@@@@@@.    @@@@           @@@@    .@@@@@@/.  ,&@@@@@@  
+  @@@@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@@@ 
+ @@@@@@@@@@@@@@@@@@*   @@@@@@@@@@@@@@@@@@@@@@@@@   *@@@@@@@@@@@@@@@@@@
+ @@@@@@@@@@@@@@@@@@   (@@@@@@@@@@@@@@@@@@@@@@@@@/   @@@@@@@@@@@@@@@@@@
+ @@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@
+ @@@@@@@@@@@@@@@@@%   @@@@@@@@@@@@@@@@@@@@@@@@@@@   %@@@@@@@@@@@@@@@@@
+ @@@@@@@@@@@@@@@@@/   @@@@@@@@@@@@@@@@@@@@@@@@@@@   #@@@@@@@@@@@@@@@@@
+  ,@@@@@@@@@@@@@@@*   @@@@@@@@@@@@@@@@@@@@@@@@@@@   (@@@@@@@@@@@@@@@* 
+                      #@@@@@@@@@@@@@@@@@@@@@@@@@%                                                                          
+            ";
+            Console.WriteLine(people);
+        }
+
+        public PersonArrayRenderer(IPersonArrayCreator personArrayCreator)
+        {
+            this.personArrayCreator = personArrayCreator;
+        }
+
+        public void RenderPersonArray()
+        {
+            GenerateImage();
+            Person[] person = personArrayCreator.CreateArray();
+            Console.WriteLine("\n  Names List:\n");
+            for (int i = 0; i < personArrayCreator.NumberOfArrayEntries; i++)
+            {
+                Console.WriteLine("  {0}.  {1}", (i + 1), person[i].ToString());
+            }
+        }
+    }
+
+    interface IPersonArrayCreator
+    {
+        public int NumberOfArrayEntries { get; set; }
+        public Person[] CreateArray();
+    }
+
+    class DefaultPersonArrayCreator : IPersonArrayCreator
+    {
+        public int NumberOfArrayEntries { get; set; }
+
+        public DefaultPersonArrayCreator(int numberOfArrayEntries)
+        {
+            this.NumberOfArrayEntries = numberOfArrayEntries;
+        }
+
+        public Person[] CreateArray()
+        {
+            Person[] person = new Person[this.NumberOfArrayEntries];
+
+            for (int i = 0; i < this.NumberOfArrayEntries; i++)
+            {
+                string firstName = UserInputValidator.ValidateUserInput("first name");
+                string lastName = UserInputValidator.ValidateUserInput("last name");
+                Console.Clear();
+
+                person[i] = new Person()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                };
+            }
+            return person;
+        }
+    }
+
+    class ListPersonNames
+    {
+        public ListPersonNames()
+        {
+            IPersonArrayCreator defaultPersonArrayCreator = new DefaultPersonArrayCreator(3); // length of array = 3
+            PersonArrayRenderer personArrayRenderer = new PersonArrayRenderer(defaultPersonArrayCreator);
+            personArrayRenderer.RenderPersonArray();
+
+            Console.Write("\nPress any key to return...");
+            Console.ReadKey();
+
+            MainMenu myMainMenu = new MainMenu();
+            myMainMenu.Start();
+        }
+    }
+}
